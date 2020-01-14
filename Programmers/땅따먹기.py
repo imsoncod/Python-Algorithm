c46688172b480facdd7a89f86dd2ede0072fe204
imsoncod <48934537+imsoncod@users.noreply.github.com>
2020-01-14 20:59:14 +0900

    Level2

---
+++ Programmers/땅따먹기.py
@@ -0,0 +1,8 @@
+def solution(land):
+    total = 0
+    for i in range(len(land)-1):
+        land[i+1][0] += max(land[i][1], land[i][2], land[i][3])
+        land[i+1][1] += max(land[i][0], land[i][2], land[i][3])
+        land[i+1][2] += max(land[i][0], land[i][1], land[i][3])
+        land[i+1][3] += max(land[i][0], land[i][1], land[i][2])
+    return max(land[-1])
