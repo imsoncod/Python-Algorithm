a2972c60ba95d8fa626c17a7b7dfe34226af7c78
imsoncod <48934537+imsoncod@users.noreply.github.com>
2020-01-26 17:28:21 +0900

    Level3

---
+++ Programmers/예산2.py
@@ -0,0 +1,15 @@
+def solution(budgets, M):
+    ans = 0
+    budgets.sort()
+    left,right = 0,budgets[-1]
+    while left<=right:
+        result = 0
+        pivot = (left+right)//2
+        for i in budgets:
+            result += min(pivot, i)
+        if result>M:
+            right = pivot-1
+        else:
+            ans = max(ans,pivot)
+            left = pivot+1
+    return ans
