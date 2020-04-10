eb5dffc397cef2d8006ca878df8fb6175357778f
imsoncod <iustron@naver.com>
2020-04-10 21:55:52 +0900

    2019 카카오 개발자 겨울 인턴십

--- Programmers/호텔 방 배정.py
+++ Programmers/호텔 방 배정.py
@@ -1,8 +1,9 @@
 import sys
 import math
-sys.setrecursionlimit(123456789)
+sys.setrecursionlimit(123456789) #재귀함수 호출횟수 제한
 
 def find_empty_room(num, room):
+    #방이 비어있다면
     if num not in room:
         room[num] = num + 1
         return num
@@ -14,6 +15,7 @@ def find_empty_room(num, room):
 def solution(k, room_number):
     ans = []
     room = dict()
+    #room의 key는 가장가까운 비어있는 방의 번호(value)를 가리킴
 
     for num in room_number:
         empty_room = find_empty_room(num, room)
