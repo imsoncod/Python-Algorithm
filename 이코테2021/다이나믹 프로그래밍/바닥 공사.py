7551b088ca52ca199dc8e501e75a69f06122a4d0
imsoncod <iustron@naver.com>
2020-11-13 15:20:50 +0900

    다이나믹 프로그래밍

---
+++ 이코테2021/다이나믹 프로그래밍/바닥 공사.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+dp = [0]*(n+1)
+
+for i in range(1, n+1):
+    if i == 1:
+        dp[i] = 1
+    elif i == 2:
+        dp[i] = 3
+    else:
+        #점화식
+        dp[i] = (2*dp[i-2] + dp[i-1])%796796
+
+print(dp[n])
